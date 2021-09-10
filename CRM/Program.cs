7906494ffef25d61f0b1e7e4e435aff4634ab10c
Vigor-Lundkvist <vigor.lundkvist@live.se>
2021-09-10 13:01:03 +0200

    Har skapat en meny som kommer utnytja if satser i en while loop.

--- CRM/Program.cs
+++ CRM/Program.cs
@@ -8,12 +8,47 @@ namespace CRM
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Customer myCustomer; //vi berättar att vi från stack tänker använda ett customer objekt
+            myCustomer = new Customer(); //här hamnar ett unikt objekt i minnet (Heap)
+
+
+
+            //skapa en meny 1-Skapa ny kund, 2-Visa antal kunder, 3-Visa lista över alla kunder, 4-Avsluta
+            while (true)
+            { 
+                Console.WriteLine("Skriv ett av följande tal:\n" +
+                              "1-Skapa en ny Kund \n" + 
+                              "2-Visa antal kunder \n" +
+                              "3-Visa lista över alla kunder \n" +
+                              "4-Avsluta");
+                 string word = Console.ReadLine();
+
+
+                if (word == "1") 
+                {
+                 
+                }
+                if (word == "2") 
+                {
+
+                }
+                if (word == "3")
+                {
+
+                }
+                if (word == "4")
+                {
+                    break;
+                }
+            }
+               
         }
     }
 }
-public class Customer // är nu en datatyp
+public class Customer                   // är nu en datatyp
 {
-    public string Name { get; set; } //Namn på kund
+    public string Name { get; set; }    //Namn på kund
     public string Address { get; set; } //Adress på kund
-    public int Age { get; set; } // Ålder på kund
+    public int Age { get; set; }        // Ålder på kund
 }
