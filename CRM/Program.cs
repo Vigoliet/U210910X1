4729cd58eed0bd3434382352779bc7ebf8729fc8
Vigor-Lundkvist <vigor.lundkvist@live.se>
2021-09-10 12:18:23 +0200

    Initial commit, lagt till text fil och börjat skapa klassen.

---
+++ CRM/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+public class Customer // är nu en datatyp
+{
+    public string Name { get; set; } //Namn på kund
+    public string Address { get; set; } //Adress på kund
+    public int Age { get; set; } // Ålder på kund
+}
